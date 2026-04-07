1f12cf31903ec1975799f92aea486cca14b47402
bitsoil37-creator <bitsoil37@gmail.com>
2026-04-07 18:50:13 +0800

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ import { getDatabase, ref, onValue, set } from "https://www.gstatic.com/firebase
 
 /* --- Firebase Config --- */
 const firebaseConfig = {
-  databaseURL: "https://soilbitchina-default-rtdb.firebaseio.com/"
+  databaseURL: "https://finalcloud-c996e-default-rtdb.firebaseio.com/"
 };
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
